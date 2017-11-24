38d476ddd874fd5387768503c740b1f102ce0489
John Claro <john.claro@distilled.ie>
2017-11-24 14:35:04 +0000

    Update to take in a person and a job

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import logging
+from random import randint
 
 from flask import Flask, render_template
-from flask_ask import Ask, question
+from flask_ask import Ask, question, statement, session
 
 
 app = Flask(__name__)
@@ -14,6 +15,13 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 def welcome():
     welcome_msg = render_template('welcome')
     return question(welcome_msg)
+
+
+@ask.intent('JobIntent', convert={'person': str})
+def next_round(person):
+    job = 'Professional athlete'
+    round_msg = render_template('job', person=person, job=job)
+    return statement(round_msg)
 
 
 if __name__ == '__main__':
